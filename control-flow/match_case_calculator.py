061a0e86c1244f87b6912fc0a106f33402879907
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-16 07:45:17 +0200

    feat: add simple calculator using match-case for arithmetic operations

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+  case "+":
+    print(num1 + num2)
+  case "-":
+    print(num1 - num2)
+  case "*":
+    print(num1 * num2)
+  case "/": 
+    print(num1 / num2)
+  case _:
+    print("Invalid number")
