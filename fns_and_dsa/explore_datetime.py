48eeb77823846030b23e551b9b74d705a9dee873
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-22 13:00:31 +0200

    feat: add current date display and future date calculation using timedelta

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+  current_date = datetime.now()
+  print("Current date and time:", current_date)
+  
+display_current_datetime()
+
+number_of_days = int(input("Enter number of days to add: "))
+
+def calculate_future_days():
+  future_date = datetime.now() + timedelta(days=number_of_days)
+  print(f"Date after {number_of_days} days will be:", future_date)
+
+calculate_future_days()
