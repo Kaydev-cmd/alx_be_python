c635aaa08b1192afc7b33fc54a09e41e7a66eeaa
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-26 12:14:14 +0200

    fix: add colon to temperature input prompt

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 temperature = int(input("Enter the temperature to convert: "))
-temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F) ").strip().upper()
+temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
